7310b65a5ed42b3c430a77ab29410f37479d049c
Ole Demay <olesyademy@gmail.com>
2022-10-26 18:02:37 +0200

    Cases dissapper, and bank offer implemented

--- script.js
+++ script.js
@@ -3,18 +3,19 @@ const briefcase = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
   23, 24, 25, 26,
 ];
-
+let totalMoney = 0;
+let moneyLost = 0;
+const bankOffer = document.getElementById("bank-offer");
+const priceOffer = document.getElementById("bank-price-offer");
 const prizeDisplay = [...document.querySelectorAll(".value")];
 const briefcaseDisplay = document.querySelectorAll(".briefcase");
 
 shuffle(prizeDisplay);
-
 prizeDisplay.forEach((prize) => {
   prizeMoney.push(Number(prize.textContent.slice(1)));
+  totalMoney += Number(prize.textContent.slice(1));
 });
-
-// console.log(prizeMoney);
-
+const outOfVault = [];
 const vault = briefcase.map((briefcase, i) => {
   return {
     briefcaseNum: briefcase,
@@ -25,17 +26,27 @@ const vault = briefcase.map((briefcase, i) => {
 });
 // console.log(vault);
 
-let notOpenedCount = prizeDisplay.length;
-let prizeTotal = 0;
-let bankerOfferAmount;
-let selectedValues = [];
+let notOpenedCount = outOfVault.length;
 
-// to add click to all briefcases
-vault.forEach((briefcase) => {
+vault.forEach((briefcase, index) => {
   briefcase.briefcaseElem.addEventListener("click", (e) => {
+    outOfVault.push(vault.splice(index, 1)[0]);
     briefcase.briefcaseElem.style.visibility = "hidden";
-    briefcase.valueElem.style.backgroundColor = "grey";
-    console.log(briefcase);
+    briefcase.valueElem.classList.add("out-of-vault");
+    moneyLost += briefcase.value;
+
+    if (
+      outOfVault.length === 7 ||
+      outOfVault.length === 14 ||
+      outOfVault.length === 19
+    ) {
+      // Calculate the offer of the bank
+      let currentOffer = Math.round((totalMoney - moneyLost) / vault.length);
+      priceOffer.textContent = currentOffer;
+
+      //Display the modal
+      bankOffer.showModal();
+    }
   });
 });
 
