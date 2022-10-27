9917857f2e797649f5190515f5dd2da5045cbe88
Ole Demay <olesyademy@gmail.com>
2022-10-27 15:45:43 +0200

    Game is done

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-const prizeMoney = [];
+const bankOffer = document.getElementById("bank-offer");
+const offerMsg = document.querySelector("#offer-msg");
+const priceOffer = document.getElementById("bank-price-offer");
+const offerBtns = document.querySelectorAll(".offer.buttons");
+const restartBtn = document.querySelector("#restart");
+const prizeDisplay = [...document.querySelectorAll(".value")];
+const briefcaseDisplay = document.querySelectorAll(".briefcase");
+const finished = document.getElementById("finished");
+
+let prizeMoney = [];
 const briefcase = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
   23, 24, 25, 26,
@@ -6,12 +15,8 @@ const briefcase = [
 
 let totalMoney = 0;
 let moneyLost = 0;
+let currentOffer = null;
 
-const bankOffer = document.getElementById("bank-offer");
-const priceOffer = document.getElementById("bank-price-offer");
-const prizeDisplay = [...document.querySelectorAll(".value")];
-const briefcaseDisplay = document.querySelectorAll(".briefcase");
-const finished = document.getElementById("finished");
 shuffle(prizeDisplay);
 prizeDisplay.forEach((prize) => {
   prizeMoney.push(Number(prize.textContent.slice(1)));
@@ -19,8 +24,8 @@ prizeDisplay.forEach((prize) => {
 });
 
 let chosenBriefcase = null;
-const outOfVault = [];
-const vault = briefcase.map((briefcase, i) => {
+let outOfVault = [];
+let vault = briefcase.map((briefcase, i) => {
   return {
     briefcaseNum: briefcase,
     briefcaseElem: briefcaseDisplay[i],
@@ -44,7 +49,6 @@ vault.forEach((briefcase) => {
         moneyLost += briefcase.value;
       }
 
-      console.log(vault);
       if (vault.length === 0) {
         console.log("you won ", chosenBriefcase.value);
       }
@@ -56,30 +60,60 @@ vault.forEach((briefcase) => {
         outOfVault.length === 24
       ) {
         // Calculate the offer of the bank
-        let currentOffer = Math.round(
+        currentOffer = Math.round(
           (totalMoney - moneyLost) / (vault.length + 1)
         );
         priceOffer.textContent = currentOffer;
 
+        if (outOfVault.length === 24) {
+          offerMsg.textContent = "Do you want to keep your case or accept: $";
+        }
         //Display the modal
         bankOffer.showModal();
-        // } else if (outOfVault === 24) {
-        //   winnings.textContent = winningBox;
-        //   lastDeal.style.display = "block";
-        // }
-        //   });
       }
     },
     { once: true }
   );
 });
-noDeal.addEventListener("click", function () {
-  bankOffer.close();
+
+restartBtn.addEventListener("click", () => {
+  totalMoney = 0;
+  moneyLost = 0;
+  currentOffer = null;
+  prizeMoney = [];
+
+  shuffle(prizeDisplay);
+  prizeDisplay.forEach((prize) => {
+    prizeMoney.push(Number(prize.textContent.slice(1)));
+    totalMoney += Number(prize.textContent.slice(1));
+  });
 });
+
+noDeal.addEventListener("click", function () {
+  if (vault.length === 1) {
+    if (chosenBriefcase.value > currentOffer) {
+      offerMsg.textContent = "Congratulations! You have won: $";
+      currentOffer.textContent = chosenBriefcase.value;
+    } else {
+      offerMsg.textContent =
+        "You lost, The banker beat you! you should have taken the $";
+    }
+    finishButtons();
+  } else {
+    bankOffer.close();
+  }
+});
+
 Deal.addEventListener("click", function () {
-  bankOffer.close();
-  winnings.textContent = bankOffer.textContent;
-  finish();
+  if (chosenBriefcase.value > currentOffer) {
+    offerMsg.textContent =
+      "You lost, The banker beat you! your case was worth $" +
+      chosenBriefcase.value +
+      " and you accepted $";
+  } else {
+    offerMsg.textContent = "Congratulations! You have won: $";
+  }
+  finishButtons();
 });
 
 // to shiffle
@@ -89,6 +123,10 @@ function shuffle(array) {
     [array[i], array[j]] = [array[j], array[i]];
   }
 }
-function finish() {
-  finished.classList.remove("hidden");
+
+function finishButtons() {
+  offerBtns.forEach((button) => {
+    button.classList.add("hidden");
+  });
+  restartBtn.classList.remove("hidden");
 }
